421621d23b61ebb5b6b6488355f9a2e4f227e6c2
Mark AI <chanthavong_12@hotmail.com>
2021-12-23 11:43:28 +0700

    add form

--- user/add.php
+++ user/add.php
@@ -3,11 +3,32 @@ require_once '../config/config.php';
 require_once '../config/db.php';
 
 // sql select
-$sql = 'SELECT * FROM departments order by id desc';
+$sql = 'SELECT id,name FROM departments order by id desc';
 // run sql
 $results = $db->query($sql);
 // get all data to $rows 
 $rows = $results->fetch_all(MYSQLI_ASSOC);
+
+// get input from user 
+//print_r($_POST);
+if(isset($_POST['submit'])){
+  $fullname = $_POST['fullname'];
+  $email = $_POST['email'];
+  $dep_id = $_POST['dep_id'];
+  $password = $_POST['password'];
+  $role = $_POST['role'];
+  $address = $_POST['address'];
+  $id = time();
+
+  $password = password_hash($password,PASSWORD_DEFAULT);
+
+  $sql = "INSERT INTO users (id,fullname,email,dep_id,password,role,address) 
+   VALUES ($id,'$fullname','$email','$dep_id','$password','$role','$address')";
+  echo $sql;
+  $db->query($sql);
+  echo 'add user success';
+  
+}
 ?>
 
 
@@ -23,7 +44,7 @@ $rows = $results->fetch_all(MYSQLI_ASSOC);
   </select>
   
   <h5>Department</h5>
-  <select>
+  <select name="dep_id">
     <?php
     foreach ($rows as $row) {
       echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
@@ -31,7 +52,7 @@ $rows = $results->fetch_all(MYSQLI_ASSOC);
     ?>
   </select>
    
-  <textarea name="address"  name=""  placeholder="Address"></textarea>
+  <textarea name="address"  placeholder="Address"></textarea>
   
   <button type="submit"  name="submit" value="Add user">submit</button>
   <a href="<?php echo BASE_URL ?>/user">Cancel</a>
