3ef7c5114d082b43a69cd5a9caaa8ad6916ec9d3
Mark AI <chanthavong_12@hotmail.com>
2022-01-04 11:12:58 +0700

    add user data and check email

--- user/add.php
+++ user/add.php
@@ -6,28 +6,38 @@ require_once '../config/db.php';
 $sql = 'SELECT id,name FROM departments order by id desc';
 // run sql
 $results = $db->query($sql);
-// get all data to $rows 
+// get all data to $rows
 $rows = $results->fetch_all(MYSQLI_ASSOC);
 
-// get input from user 
+$ms = '';
+
+// get input from user
 //print_r($_POST);
-if(isset($_POST['submit'])){
-  $fullname = $_POST['fullname'];
-  $email = $_POST['email'];
-  $dep_id = $_POST['dep_id'];
-  $password = $_POST['password'];
-  $role = $_POST['role'];
-  $address = $_POST['address'];
-  $id = time();
+if (isset($_POST['submit'])) {
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $dep_id = $_POST['dep_id'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+    $address = $_POST['address'];
+    $id = time();
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
-  $password = password_hash($password,PASSWORD_DEFAULT);
+    // sql check email
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $results = $db->query($sql); // run sql
+    $row = $results->num_rows; 
+    if ($row > 0) {
+        $ms = 'ມີອີເມລນີ້ແລ້ວ ກະລຸນາໃຊ້ ອີເມລອື່ນ';
+    } else {
+       // insert user
+        $sql = "INSERT INTO users (id,fullname,email,dep_id,password,role,address) VALUES (?,?,?,?,?,?,?)";
+        $stmt = $db->prepare($sql);
+        $stmt->bind_param('ississs', $id, $fullname, $email, $dep_id, $password, $role, $address);
+        $stmt->execute();
+        $ms = 'add user success';
+    }
 
-  $sql = "INSERT INTO users (id,fullname,email,dep_id,password,role,address) 
-   VALUES ($id,'$fullname','$email','$dep_id','$password','$role','$address')";
-  echo $sql;
-  $db->query($sql);
-  echo 'add user success';
-  
 }
 ?>
 
@@ -42,18 +52,22 @@ if(isset($_POST['submit'])){
     <option value="manager">Manager</option>
     <option value="admin">Super admin</option>
   </select>
-  
+
   <h5>Department</h5>
   <select name="dep_id">
     <?php
-    foreach ($rows as $row) {
-      echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
-    }
-    ?>
+foreach ($rows as $row) {
+    echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
+}
+?>
   </select>
-   
+
   <textarea name="address"  placeholder="Address"></textarea>
-  
+  <div class="alert">
+     <?php
+      echo $ms;
+    ?>
+  </div>
   <button type="submit"  name="submit" value="Add user">submit</button>
   <a href="<?php echo BASE_URL ?>/user">Cancel</a>
 </form>
@@ -66,5 +80,11 @@ if(isset($_POST['submit'])){
   form input, form select, form textarea{
     display: block;
     margin-bottom: 10px;
+  }
+  .alert {
+    background: #f00;
+    color: #fff;
+    padding: 10px;
+    border-radius: 5px;
   }
 </style>
