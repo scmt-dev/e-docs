3903a999b0a133cce10ca804b0d70de390380f21
Mark AI <chanthavong_12@hotmail.com>
2022-01-26 11:29:24 +0700

    add form login ui

--- header.php
+++ header.php
@@ -9,7 +9,10 @@ require_once 'config/config.php';
     <meta name="viewport" 
      content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>/assets/css/style.css">
-    
+    <!-- CSS only -->
+  <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo BASE_URL ?>/assets/css/style.css">
+
 </head>
 <body>
